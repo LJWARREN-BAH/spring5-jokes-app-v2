d2dbb462b7d5d08ffe085313a713b2f955d54b46
Janie Warren <108426837+LJWARREN-BAH@users.noreply.github.com>
2022-08-09 12:52:10 -0400

    Created Jokecontroller. Closes #4

---
+++ src/main/java/warren/janie/spring5jokes/controllers/JokeController.java
@@ -0,0 +1,24 @@
+package warren.janie.spring5jokes.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.annotation.RequestScope;
+import warren.janie.spring5jokes.services.JokeService;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return  "index";
+    }
+}
