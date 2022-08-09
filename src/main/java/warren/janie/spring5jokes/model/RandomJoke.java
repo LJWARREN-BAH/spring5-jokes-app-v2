29e888374859b4c2498ef3653fcdf05b03bedf0d
Janie Warren <108426837+LJWARREN-BAH@users.noreply.github.com>
2022-08-09 12:22:18 -0400

    Created Springboot Project. Closes #1

---
+++ src/main/java/warren/janie/spring5jokes/model/RandomJoke.java
@@ -0,0 +1,9 @@
+package warren.janie.spring5jokes.model;
+
+import warren.janie.spring5jokes.services.GetRandomJokeService;
+
+public class RandomJoke {
+
+    GetRandomJokeService getRandomJokeService=GetRandomJokeService.newInstance();
+    getRandomeJokeService.printJoke();
+}
---
+++ src/main/java/warren/janie/spring5jokes/services/GetRandomJokeService.java
@@ -0,0 +1,10 @@
+package warren.janie.spring5jokes.services;
+
+import warren.janie.spring5jokes.model.RandomJoke;
+
+public interface GetRandomJokeService {
+
+    public void printRandomJoke();
+
+
+}
